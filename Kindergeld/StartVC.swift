4b121a04831fd83a4d37ccbb7cc733cb0ddc3a4e
Frantisek Varadzin <varadzin@gmail.com>
2021-08-21 15:54:45 +0200

    Setting Language Buttons on StartVC

--- Kindergeld/StartVC.swift
+++ Kindergeld/StartVC.swift
@@ -21,6 +21,7 @@ class StartVC: UIViewController {
         configTitle()
         configSubTitle()
         configImages()
+        configLanguageButtons()
     }
     
     
@@ -90,10 +91,92 @@ class StartVC: UIViewController {
             babyImg.widthAnchor.constraint(equalToConstant: 245),
             babyImg.heightAnchor.constraint(equalToConstant: 240)
         ])
+    }
+    
+    func configLanguageButtons() {
+        //Setting Button - SlovakiaFlag - chosed Slovak Language
+        let SKBtn = UIButton()
+        view.addSubview(SKBtn)
+        SKBtn.setBackgroundImage(UIImage(named: "SlovakiaFlag"), for: .normal)
         
         
+        SKBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            SKBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            SKBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            SKBtn.widthAnchor.constraint(equalToConstant: 80),
+            SKBtn.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        let SKLabel = UILabel()
+        view.addSubview(SKLabel)
+        SKLabel.text = "Slovensky"
+        SKLabel.textColor = .label
+        SKLabel.font = UIFont(name: "Times New Roman", size: 14)
+        SKLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            SKLabel.bottomAnchor.constraint(equalTo: SKBtn.topAnchor, constant: -5),
+            SKLabel.leadingAnchor.constraint(equalTo: SKBtn.leadingAnchor, constant: 10)
+        ])
+        
+        //Setting Button - GEFlag - chosed Germany Language
+        let GEBtn = UIButton()
+        view.addSubview(GEBtn)
+        GEBtn.setBackgroundImage(UIImage(named: "GermanyFlag"), for: .normal)
+        
+        
+        GEBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            GEBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            GEBtn.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: -40),
+            GEBtn.widthAnchor.constraint(equalToConstant: 80),
+            GEBtn.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        let GELabel = UILabel()
+        view.addSubview(GELabel)
+        GELabel.text = "Deutsch"
+        GELabel.textColor = .label
+        GELabel.font = UIFont(name: "Times New Roman", size: 14)
+        GELabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            GELabel.bottomAnchor.constraint(equalTo: GEBtn.topAnchor, constant: -5),
+            GELabel.leadingAnchor.constraint(equalTo: GEBtn.leadingAnchor, constant: 18)
+        ])
+        
+        //Setting Button - GBFlag - chosed English Language
+        let GBBtn = UIButton()
+        view.addSubview(GBBtn)
+        GBBtn.setBackgroundImage(UIImage(named: "GBFlag"), for: .normal)
+        
+        
+        GBBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            GBBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            GBBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            GBBtn.widthAnchor.constraint(equalToConstant: 80),
+            GBBtn.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        let GBLabel = UILabel()
+        view.addSubview(GBLabel)
+        GBLabel.text = "English"
+        GBLabel.textColor = .label
+        GBLabel.font = UIFont(name: "Times New Roman", size: 14)
+        GBLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            GBLabel.bottomAnchor.constraint(equalTo: GBBtn.topAnchor, constant: -5),
+            GBLabel.leadingAnchor.constraint(equalTo: GBBtn.leadingAnchor, constant: 18)
+        ])
         
     }
+    
     
     
 }
