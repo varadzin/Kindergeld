fc7dd6aff0fefc3181eebe734c897eba1f0418ad
Frantisek Varadzin <varadzin@gmail.com>
2021-08-21 12:50:45 +0200

    Initial Commit

---
+++ Kindergeld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kindergeld
+//
+//  Created by Frantisek Varadzin on 21/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
