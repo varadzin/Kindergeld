bd555e8bbfde7722816338877b03fa747c6bad3c
Frantisek Varadzin <varadzin@gmail.com>
2021-08-22 13:46:05 +0200

    Setting InfoVC Adding Text + Explanation Images

--- Kindergeld/Controller/InfoVC.swift
+++ Kindergeld/Controller/InfoVC.swift
@@ -7,19 +7,23 @@
 
 import UIKit
 
-class InfoVC: UIViewController {
+class InfoVC: UIViewController, UIScrollViewDelegate {
     
     let titleKG = UILabel()
     let subTitle = UILabel()
-    let textField = UITextField()
+    let textField = UILabel()
+    let scrollView = UIScrollView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configVC()
+        configScrollView()
         configTitle()
         configSubTitle()
         configTextField()
+        configBottomInstructions()
+       
 
     }
     
@@ -27,9 +31,26 @@ class InfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = true
       }
+    
+    func configScrollView() {
+        view.addSubview(scrollView)
+//        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.frame = view.bounds
+        scrollView.contentSize = CGSize(width: view.frame.size.width, height:view.frame.size.height)
+        scrollView.alwaysBounceVertical = true
+
+        
+//        NSLayoutConstraint.activate([
+//            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+//            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+//                    ])
+    }
+    
   
     func configTitle() {
-        view.addSubview(titleKG)
+        scrollView.addSubview(titleKG)
         
         titleKG.text = "Kindergeld"
         titleKG.font = UIFont(name: "Times New Roman", size: 28)
@@ -38,14 +59,16 @@ class InfoVC: UIViewController {
         titleKG.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            titleKG.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
-            titleKG.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10)
+            titleKG.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: -30),
+            
+            titleKG.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            titleKG.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 15/16)
  
         ])
     }
     
     func configSubTitle() {
-        view.addSubview(subTitle)
+        scrollView.addSubview(subTitle)
         
         subTitle.text = "čo potrebuješ vedieť"
         subTitle.font = UIFont(name: "Times New Roman", size: 18)
@@ -58,28 +81,111 @@ class InfoVC: UIViewController {
         
         NSLayoutConstraint.activate([
             subTitle.topAnchor.constraint(equalTo: titleKG.bottomAnchor, constant: 5),
-            subTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            subTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            subTitle.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            subTitle.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 15/16)
         ])
     }
     
     func configTextField() {
-        view.addSubview(textField)
+        scrollView.addSubview(textField)
         
-        textField.backgroundColor = .systemGray6
+        textField.font = UIFont(name: "Times New Roman", size: 18)
+        textField.numberOfLines = 0
+        textField.text = """
+Pomocou tejto aplikácie vieš vyplniť formuláre pre žiadosť o prídavky na deti v Nemecku. O prídavky na deti možeš požiadať aj sám/sama. Nepotrebuješ na to žiadnu agentúru, ktorej zbytočne zaplatíš desiatky €. To čo zadáš pri vypĺňaní formuláru sa ti zobrazí v žiadosti.
+
+Podmienky používania aplikácie Kindergeld:
+
+Autor aplikácie nenesie zodpovednosť za akékoľvek škody, straty alebo ujmy spôsobené používaním aplikácie, najmä za škodu spôsobenú nesprávnym vyplnením formulárov, prípadne uvedením nepravdivých údajov pri žiadosti o prídavky.
+
+Ochrana osobných údajov:
+Všetky údaje zadané užívateľom ostávajú v telefóne. Aplikácia sa nepripája na internet a neodosiela žiadne dáta. Aplikácia nezbiera ani nikomu neposkytuje žiadne údaje. Po vyplnení formulára si vieš hotové PDF vytlačiť z telefónu, alebo preniesť bezpečne do počítača a následne z počítača vytlačiť. Používaním aplikácie dávaš najavo súhlas s podmienkami používania.
+
+"""
         
         textField.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
         
             textField.topAnchor.constraint(equalTo: subTitle.bottomAnchor, constant: 10),
-            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            textField.heightAnchor.constraint(equalToConstant: 500)
+            textField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            textField.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 15/16)
+        
         ])
         
     }
     
-    
+    func configBottomInstructions() {
+        let infoImg = UIImageView()
+        scrollView.addSubview(infoImg)
+        
+        infoImg.image = UIImage(named: "InfoSK")
+        infoImg.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            infoImg.bottomAnchor.constraint(equalTo: textField.bottomAnchor, constant: 80),
+            infoImg.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 8),
+            infoImg.widthAnchor.constraint(equalToConstant: 65),
+            infoImg.heightAnchor.constraint(equalToConstant: 59)
+         
+        ])
+        
+        let fillFormImg = UIImageView()
+        scrollView.addSubview(fillFormImg)
+        
+        fillFormImg.image = UIImage(named: "fillFormSK")
+        fillFormImg.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            fillFormImg.bottomAnchor.constraint(equalTo: textField.bottomAnchor, constant: 80),
+            fillFormImg.leadingAnchor.constraint(equalTo: infoImg.trailingAnchor, constant: 7),
+            fillFormImg.widthAnchor.constraint(equalToConstant: 65),
+            fillFormImg.heightAnchor.constraint(equalToConstant: 59)
+         
+        ])
+        
+        let payDaysImg = UIImageView()
+        scrollView.addSubview(payDaysImg)
+        
+        payDaysImg.image = UIImage(named: "payDaysSK")
+        payDaysImg.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            payDaysImg.bottomAnchor.constraint(equalTo: textField.bottomAnchor, constant: 80),
+            payDaysImg.leadingAnchor.constraint(equalTo: fillFormImg.trailingAnchor, constant: 12),
+            payDaysImg.widthAnchor.constraint(equalToConstant: 65),
+            payDaysImg.heightAnchor.constraint(equalToConstant: 59)
+         
+        ])
+        
+        let settingsImg = UIImageView()
+        scrollView.addSubview(settingsImg)
+        
+        settingsImg.image = UIImage(named: "settingsSK")
+        settingsImg.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsImg.bottomAnchor.constraint(equalTo: textField.bottomAnchor, constant: 80),
+            settingsImg.leadingAnchor.constraint(equalTo: payDaysImg.trailingAnchor, constant: 10),
+            settingsImg.widthAnchor.constraint(equalToConstant: 65),
+            settingsImg.heightAnchor.constraint(equalToConstant: 48)
+         
+        ])
+        
+        let pdfImg = UIImageView()
+        scrollView.addSubview(pdfImg)
+        
+        pdfImg.image = UIImage(named: "pdfSK")
+        pdfImg.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pdfImg.bottomAnchor.constraint(equalTo: textField.bottomAnchor, constant: 80),
+            pdfImg.leadingAnchor.constraint(equalTo: settingsImg.trailingAnchor, constant: 9),
+            pdfImg.widthAnchor.constraint(equalToConstant: 65),
+            pdfImg.heightAnchor.constraint(equalToConstant: 75)
+         
+        ])
+        
+    }
     
 }
--- Kindergeld/SceneDelegate.swift
+++ Kindergeld/SceneDelegate.swift
@@ -21,7 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = UINavigationController.init(rootViewController: StartVC())
-                window?.makeKeyAndVisible()
+
+        window?.makeKeyAndVisible()
  
         
         
