a2e4842707cc3a26481e529c3a00fa5b14a241d2
Frantisek Varadzin <varadzin@gmail.com>
2021-08-26 18:11:07 +0200

    Setting Settings View

--- Kindergeld/Controller/SettingsVC.swift
+++ Kindergeld/Controller/SettingsVC.swift
@@ -9,15 +9,37 @@ import UIKit
 
 class SettingsVC: UIViewController {
 
+    var titleKG = UILabel()
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configVC()
 
     }
-    
     func configVC() {
-        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.isNavigationBarHidden = true
         view.backgroundColor = .systemBackground
     }
+    
+    func configTitle() {
+        view.addSubview(titleKG)
+        
+        titleKG.text = "Nastavenia"
+        titleKG.font = UIFont(name: "Times New Roman", size: 28)
+        titleKG.textColor = .secondaryLabel
+        
+        titleKG.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleKG.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            titleKG.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15)
+            
+            
+        ])
+    }
+    
+
     
 }
