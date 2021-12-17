4e6b7fa2a5a9a8123c56e15d535cbb86f4053c69
Frantisek Varadzin <varadzin@gmail.com>
2021-12-17 19:31:58 +0100

    refactoring FormVC6

---
+++ Kindergeld/Controller/FormsViewControllers/FormVC6 + Ext.swift
@@ -0,0 +1,74 @@
+//
+//  FormVC6 + Ext.swift
+//  Kindergeld
+//
+//  Created by Frantisek Varadzin on 17.12.21.
+//
+
+import UIKit
+
+extension FormVC6 {
+    
+    func layoutFormVC6() {
+        
+        helpButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            helpButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            helpButton.widthAnchor.constraint(equalToConstant: 100),
+            helpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            helpButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        titleKG.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleKG.topAnchor.constraint(equalTo: helpButton.bottomAnchor, constant: 5),
+            
+            titleKG.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleKG.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 15/16)
+            
+        ])
+        
+        subTitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            subTitle.topAnchor.constraint(equalTo: titleKG.bottomAnchor, constant: 5),
+            subTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            subTitle.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 15/16)
+        ])
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            datePicker.topAnchor.constraint(equalTo: subTitle.bottomAnchor),
+            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            datePicker.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        NSLayoutConstraint.activate([
+            spouseNationality.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 5),
+            spouseNationality.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            spouseNationality.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            spouseNationality.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: spouseNationality.bottomAnchor, constant: 15),
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            backButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -200),
+            backButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nextButton.topAnchor.constraint(equalTo: spouseNationality.bottomAnchor, constant: 15),
+            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 200),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        
+    }
+    
+    
+}
--- Kindergeld/Controller/FormsViewControllers/FormVC6.swift
+++ Kindergeld/Controller/FormsViewControllers/FormVC6.swift
@@ -12,28 +12,29 @@ class FormVC6: UIViewController {
     
     
     
-    
+    let helpButton = KGHelpButton(title: "        Help")
     let titleKG = UILabel()
     let subTitle = UILabel()
-    let spouseNameQ = KGTextField(placeholder: " Krstné meno partnera / ky ")
-    let spouseSurenameQ = KGTextField(placeholder: " Priezvisko partnera / ky ")
-    let maidenSpouseNameQ = KGTextField(placeholder: " Rodné priezvisko partnera / ky ")
-    let explainButton = KGButton(title: "Vysvetlivky - Partner / ka -  / manžel / ka")
+    let spouseNameQ = KGTextField(placeholder: " First Name of spouse ")
+    let spouseSurenameQ = KGTextField(placeholder: " Name of spouse ")
+    let maidenSpouseNameQ = KGTextField(placeholder: " Maiden Name ")
     let simpleText = KGTextLabel()
     let datePicker = UIDatePicker()
-    let spouseNationality = KGTextField(placeholder: "Štátna príslušnosť partnera/ky")
+    let spouseNationality = KGTextField(placeholder: "Nationality of spouse")
+    let backButton = KGButton(title: "<<- back")
+    let nextButton = KGButton(title: "next ->>")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configVC()
+        configHelpBtn()
         configTitle()
         configSubTitle()
-//        configSimpleText()
         configDatePicker()
         configQ6()
         configBackButton()
         configNextButton()
-        
+        layoutFormVC6()
     }
     
     
@@ -42,148 +43,88 @@ class FormVC6: UIViewController {
         navigationController?.isNavigationBarHidden = true
     }
     
+    func configHelpBtn() {
+        view.addSubview(helpButton)
+        helpButton.addTarget(self, action: #selector(showExplanation), for: .touchUpInside)
+    }
     
     
     func configTitle() {
         view.addSubview(titleKG)
-        
-        titleKG.text = "Hlavná žiadosť - (KG1)"
+        titleKG.text = "Information on the spouse or registered civil partner"
         titleKG.font = UIFont(name: "Times New Roman", size: 28)
         titleKG.textColor = .secondaryLabel
-        
-        titleKG.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            titleKG.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
-            
-            titleKG.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleKG.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 15/16)
-            
-        ])
+        titleKG.numberOfLines = 0
+        titleKG.textAlignment = .center
     }
     
     func configSubTitle() {
         view.addSubview(subTitle)
-        
-        subTitle.text = "Dátum narodenia partnera / ky"
+        subTitle.text = "Date of birth - spouse"
         subTitle.font = UIFont(name: "Times New Roman", size: 22)
         subTitle.textColor = UIColor.systemOrange
         subTitle.textAlignment = .center
         subTitle.numberOfLines = 0
-        
-        
-        subTitle.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            subTitle.topAnchor.constraint(equalTo: titleKG.bottomAnchor, constant: 5),
-            subTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            subTitle.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 15/16)
-        ])
     }
     
-
+    
+    @objc func showExplanation() {
+        let destVC = KGExplainVC()
+        destVC.title = "Help"
+        let navCon = UINavigationController(rootViewController: destVC)
+        present(navCon, animated: true)
+    }
     
     
     func configDatePicker() {
-    
+        
         view.addSubview(datePicker)
-
+        
         datePicker.datePickerMode = .date
         datePicker.preferredDatePickerStyle = .wheels
         datePicker.timeZone = NSTimeZone.local
- 
-        
-      
         datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
-        datePicker.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([
-            datePicker.topAnchor.constraint(equalTo: subTitle.bottomAnchor),
-            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            datePicker.heightAnchor.constraint(equalToConstant: 200)
-        ])
         
     }
     
     @objc func datePickerValueChanged(_ sender: UIDatePicker) {
         let dateFormatter = DateFormatter()
-        
         dateFormatter.dateFormat = "dd.MM.yyyy"
-        
         let selectedDate: String = dateFormatter.string(from: sender.date)
-        
-        print(selectedDate)
-        
-        
     }
     
     
     func configQ6() {
-         
         view.addSubview(spouseNationality)
-        
         spouseNationality.keyboardType = .alphabet
-        
-        NSLayoutConstraint.activate([
-            spouseNationality.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 5),
-            spouseNationality.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            spouseNationality.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            spouseNationality.heightAnchor.constraint(equalToConstant: 50)
-        ])
-        
-    
     }
     
     func configBackButton() {
-        let backButton = KGButton(title: "<<- späť")
         view.addSubview(backButton)
-        
-                backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
-        
-        NSLayoutConstraint.activate([
-            backButton.topAnchor.constraint(equalTo: spouseNationality.bottomAnchor, constant: 15),
-            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            backButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -200),
-            backButton.heightAnchor.constraint(equalToConstant: 50)
-        ])
-        
+        backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
     }
     
     @objc func goBack() {
         navigationController?.popViewController(animated: true)
-        
     }
     
     
     func configNextButton() {
-        let nextButton = KGButton(title: "ďalej ->>")
         view.addSubview(nextButton)
-        
         nextButton.addTarget(self, action: #selector(saveDataAndGoNext), for: .touchUpInside)
-        
-        NSLayoutConstraint.activate([
-            nextButton.topAnchor.constraint(equalTo: spouseNationality.bottomAnchor, constant: 15),
-            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 200),
-            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            nextButton.heightAnchor.constraint(equalToConstant: 50)
-        ])
-        
     }
     
-   @objc func saveDataAndGoNext() {
+    @objc func saveDataAndGoNext() {
         
-    let nextVC = FormVC7()
-    navigationController?.pushViewController(nextVC, animated: true)
-        
+        let nextVC = FormVC7()
+        navigationController?.pushViewController(nextVC, animated: true)
     }
-    
-    
 }
+
 extension FormVC6: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         saveDataAndGoNext()
         return true
     }
-    
 }
